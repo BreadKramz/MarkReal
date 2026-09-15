371ef74a797ab35015c6c3b2e681c89bb72e1ba3
Kramz <marklovegames55@gmail.com>
2026-09-15 23:04:58 +0800

    Mount portfolio theme switch

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "./display-fixes.css";
+import ThemeToggle from "./ThemeToggle.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <ThemeToggle />
   </StrictMode>,
 );
